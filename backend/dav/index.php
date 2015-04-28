520e53ce69ce68e760d216ce36e9a08212df6030
Samuli Järvelä <samuli@jarvela.com>
2015-04-28 12:48:50 +0300

    dav & basic auth

--- backend/dav/index.php
+++ backend/dav/index.php
@@ -6,7 +6,7 @@
 	$ENABLE_LOCKING = TRUE;
 	$ENABLE_BROWSER = FALSE;
 	$ENABLE_TEMPORARY_FILE_FILTER = TRUE;
-	$BASIC_AUTH = FALSE;
+	$BASIC_AUTH = TRUE;
 	//$MAX_FILE_SIZE = "10M";
 	
 	require_once("kloudspeaker_dav.php");
