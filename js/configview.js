3ee0b83049613d8d618595f649ed36b7e71e3749
sjarvela <cambba@Samulis-MBP.local>
2015-07-03 09:33:27 +0300

    config view AMD

---
+++ js/configview.js
@@ -0,0 +1,17 @@
+/**
+ * configview.js
+ *
+ * Copyright 2015- Samuli Järvelä
+ * Released under GPL License.
+ *
+ * License: http://www.kloudspeaker.com/license.php
+ */
+
+! function($, kloudspeaker) {
+
+    //"use strict";
+
+    //kloudspeaker.view.MainViewConfigView = function() {
+        
+    //}
+}(window.jQuery, window.kloudspeaker);
