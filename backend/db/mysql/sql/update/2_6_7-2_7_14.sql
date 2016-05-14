6d1061e98016e9e186ee3784064753766726f03a
sjarvela <samuli@jarvela.com>
2016-05-14 18:57:26 +0300

    update table prefix

--- backend/db/mysql/sql/update/2_6_7-2_7_14.sql
+++ backend/db/mysql/sql/update/2_6_7-2_7_14.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `{TABLE_PREFIX}item_id` ADD `level` smallint NULL;
 UPDATE `{TABLE_PREFIX}item_id` SET `level` = (LENGTH(path) - LENGTH(REPLACE(SUBSTRING(path, 1, LENGTH(path)-1), '/', '')));
 ALTER TABLE `{TABLE_PREFIX}item_id` MODIFY COLUMN `level` smallint NOT NULL;
-ALTER TABLE `item_id` ADD INDEX(`level`);
-ALTER TABLE `item_id` ADD INDEX(`path`, `level`);
+ALTER TABLE `{TABLE_PREFIX}item_id` ADD INDEX(`level`);
+ALTER TABLE `{TABLE_PREFIX}item_id` ADD INDEX(`path`, `level`);
 
 UPDATE `{TABLE_PREFIX}parameter` SET `value` = '2_7_14' where `name` = 'version';
