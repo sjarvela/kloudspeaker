7fa06b6685fe61c4d2d87c5b4893d3be47517dca
sjarvela <samuli@jarvela.com>
2016-10-11 16:09:45 +0300

    folder info data

--- backend/include/filesystem/CoreFileDataProvider.class.php
+++ backend/include/filesystem/CoreFileDataProvider.class.php
@@ -17,7 +17,7 @@ class CoreFileDataProvider {
 	}
 
 	public function init($c) {
-		$c->registerDataRequestPlugin(array("core-file-modified"), $this);
+		$c->registerDataRequestPlugin(array("core-file-modified", "folder-info"), $this);
 	}
 
 	public function getRequestData($parent, $items, $key, $requestData) {
@@ -26,6 +26,11 @@ class CoreFileDataProvider {
 			foreach ($items as $i) {
 				$result[$i->id()] = $this->env->configuration()->formatTimestampInternal($i->lastModified());
 
+			}
+		}
+		if (strcmp("folder-info", $key) === 0) {
+			foreach ($items as $i) {
+				if (!$i->isFile()) $result[$i->id()] = $this->env->filesystem()->getFolderInfo($i);
 			}
 		}
 
